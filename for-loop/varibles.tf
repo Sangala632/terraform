071db52ef0247b6f50c164f85ea576c4e36d6134
sangala632 <sangala2003@gmail.com>
2025-12-24 14:42:32 +0530

    terraform practice (clean repo)

---
+++ for-loop/varibles.tf
@@ -0,0 +1,67 @@
+    variable "ami_id" {
+    type        = string
+    description = "AMI id of devSecOps RHEL9"
+    default     = "ami-09c813fb71547fc4f"
+    }
+
+    variable "instance_type" {
+        default = "t3.micro"
+    }
+
+    variable "ec2_tags" {
+        type    = map(string)
+        default = {
+            Name    = "roboshop"
+            purpose = "varibles-demo"
+        }
+    }
+
+    variable "sg_name" {
+        default = "allow_all"
+    }
+
+    variable "sg_description" {
+        default = "allowing all ports and all traffic from internet"
+    }
+
+    variable "from_port" {
+        type    = number
+        default = 0
+    }
+
+    variable "to_port" {
+        type    = number
+        default = 0
+    }
+
+    variable "cidr_blocks" {
+        type    = list(string)
+        default = ["0.0.0.0/0"]
+    }
+
+    variable "sg_tags"{
+        default = {
+            Name = "allow_all"
+        }
+    }
+    variable "environment" {
+        default = "dev"
+    }
+
+    variable "instances" {
+        default = ["mongodb","redis","mysql","rabbitmq"]
+        /* default = {
+            mongodb = "t3.micro"
+            redis   = "t3.micro"
+            mysql   = "t3.small"
+            rabbitmq = "t3.micro"
+        } */
+    }
+
+    variable "Domain_name" {
+        default = "hellodevsecops.space"
+    }
+
+    variable "zone_id" {
+        default = "Z0168679G0Y1V181BPMR"
+    }
